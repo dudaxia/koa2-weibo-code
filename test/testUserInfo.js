b81d25cfcbf7b44682bf270d873cbbb198d7b993
dudaxia <1729453966@qq.com>
2020-05-22 15:24:20 +0800

    test: 创建blog

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -4,6 +4,11 @@
  * @author dudaxia
  */
 
+/**
+ * 【特别提示】cookie 是用户的敏感信息，此处只能是**测试**用户的cookie
+ *  每次测试用户重新登录，都需要更新此处的cookie
+ */
+
 module.exports = {
   COOKIE: 'weibo.sid=2ZdlkQYRDASdKtbGY1TnCvxYVXH86ADF; weibo.sid.sig=4G98AfaHylw-u58w-ajp2bu9y1o'
 }
